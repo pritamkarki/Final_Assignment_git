1bc2c734b069e88152bdc945cf11aabd4ef2a4b5
pritamkarki <60006016+pritamkarki@users.noreply.github.com>
2020-01-17 22:38:30 +0545

    Add files via upload

---
+++ UnitTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("UnitTesting")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("569fbfd2-c077-452f-afc8-b51add90d0ba")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTesting/UnitTest1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using ASE_Assignment;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Moveto_Test_Right()
+        {
+            var v = new CodeVallidation();
+            string[] result = v.valid("moveto 10 10");
+            Assert.AreEqual(result[0], "moveTo", result[1], "10", result[2], "10");
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void Moveto__Test_Wrong()
+        {
+            var v = new CodeVallidation();
+            string[] result = v.valid("moveto 10");
+            Assert.AreEqual(result[0], "10");
+        }
+
+        [TestMethod]
+        /*public void Drawto_Test_Right()
+        {
+            var v = new CodeVallidation();
+            string[] result = v.valid("drawto 20 30");
+            Assert.AreEqual(result[0], "drawto", result[1], "20", result[2], "30");
+
+        }
+        [TestMethod]
+        public void Drawto__Test_Wrong()
+        {
+            var v = new CodeVallidation();
+            string[] result = v.valid("drawto 20");
+            Assert.AreEqual(result[0], "1");
+        }
+*/
+
+        
+        public void Rectangleto_Test_Right()
+        {
+            var v = new CodeVallidation();
+            string[] result = v.valid("rectangle 20 30");
+            Assert.AreEqual(result[0], "rectangle", result[1], "20", result[2], "30");
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void Rectangleto__Test_Wrong()
+        {
+            var v = new CodeVallidation();
+            string[] result = v.valid("rectangle 20");
+            Assert.AreEqual(result[0], "rectangle", result[1], "20");
+        }
+
+        [TestMethod]
+        public void Cirle_Test_Right()
+        {
+            var v = new CodeVallidation();
+            string[] result = v.valid("circle 10");
+            Assert.AreEqual(result[0], "circle", result[1], "10");
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void Circle__Test_Wrong()
+        {
+            var v = new CodeVallidation();
+            string[] result = v.valid("circle 50 50");
+            Assert.AreNotSame(result[0], "1");
+        }
+
+        [TestMethod]
+        public void Triangleto_Test_Right()
+        {
+            var v = new CodeVallidation();
+            string[] result = v.valid("triangle 20 30 40");
+            Assert.AreEqual(result[0], "triangle", result[1], "20", result[2], "30", result[3], "40");
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void Triangle__Test_Wrong()
+        {
+            var v = new CodeVallidation();
+            string[] result = v.valid("20 ");
+            Assert.AreEqual(result[0], "triangle");
+        }
+
+
+    }
+}
+
